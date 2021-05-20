43294988a497cf6ca01c3b8bd78e634017c12ea7
Jacob Smith <jakesmith.seo@gmail.com>
2021-05-19 21:47:07 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+// MailChip
+(function ($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = "EMAIL";
+  ftypes[0] = "email";
+  fnames[1] = "FNAME";
+  ftypes[1] = "text";
+  fnames[2] = "LNAME";
+  ftypes[2] = "text";
+  fnames[3] = "ADDRESS";
+  ftypes[3] = "address";
+  fnames[4] = "PHONE";
+  ftypes[4] = "phone";
+  fnames[5] = "BIRTHDAY";
+  ftypes[5] = "birthday";
+})(jQuery);
+var $mcj = jQuery.noConflict(true);
