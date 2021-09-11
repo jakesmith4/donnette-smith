d786302ec5ab766bd29a68a0e898e9c26a09eb2f
Jacob Smith <jakesmith.seo@gmail.com>
2021-09-10 20:53:20 -0500

    fixed nav bar to transparant until scolled. Also changed color of nav-logo to transparent. Also changed nav-btn background to transparent

--- js/app.js
+++ js/app.js
@@ -1,41 +1,41 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
+const navbar = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('#sidebar');
+const date = document.querySelector('#date');
+// Add Fixed Class To Navbar
+window.addEventListener('scroll', function () {
   if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
+    navbar.classList.add('navbar-fixed');
   } else {
-    navbar.classList.remove("navbar-fixed");
+    navbar.classList.remove('navbar-fixed');
   }
 });
-// show sidebar
-navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
+// Show Sidebar
+navBtn.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar');
 });
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
 });
-// set year
+// Set Year
 date.innerHTML = new Date().getFullYear();
 
 // MailChip
 (function ($) {
   window.fnames = new Array();
   window.ftypes = new Array();
-  fnames[0] = "EMAIL";
-  ftypes[0] = "email";
-  fnames[1] = "FNAME";
-  ftypes[1] = "text";
-  fnames[2] = "LNAME";
-  ftypes[2] = "text";
-  fnames[3] = "ADDRESS";
-  ftypes[3] = "address";
-  fnames[4] = "PHONE";
-  ftypes[4] = "phone";
-  fnames[5] = "BIRTHDAY";
-  ftypes[5] = "birthday";
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
+  fnames[3] = 'ADDRESS';
+  ftypes[3] = 'address';
+  fnames[4] = 'PHONE';
+  ftypes[4] = 'phone';
+  fnames[5] = 'BIRTHDAY';
+  ftypes[5] = 'birthday';
 })(jQuery);
 var $mcj = jQuery.noConflict(true);
