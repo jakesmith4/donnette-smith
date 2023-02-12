4618ed91ce26b8974681450a2e4935844d2930d9
Jacob Smith <jakesmith.seo@gmail.com>
2023-02-12 09:46:03 -0600

    added proper comming soon page header

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ if (
 const about = document.querySelector('.about');
 const btns = document.querySelectorAll('.tab-btn');
 const articles = document.querySelectorAll('.content');
-about.addEventListener('click', function (e) {
+about?.addEventListener('click', function (e) {
   const id = e.target.dataset.id;
   if (id) {
     // remove selected from other buttons
